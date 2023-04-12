dcba24615b0ec584f4496e55c6013866fe828888
BereniceCastelan <jatziriten30@gmail.com>
2023-04-12 14:39:52 -0600

    Primer

--- Jatziri tenorio/ContentView.swift
+++ Jatziri tenorio/ContentView.swift
@@ -5,17 +5,56 @@
 //  Created by CEDAM09 on 12/04/23.
 //
 
+import Foundation
+
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct ContentView: View
+{
+    @State private var lampara = true
+    
+    var body: some View
+    {
+        ZStack
+        {
+            Color(.systemBlue)
+                .ignoresSafeArea()//fondo sea completamente azul
+            VStack
+            {
+                Button("este es un cambio de fondo")
+                {
+                    lampara.toggle()//para boleanos cambia su valor actiual por el opuesto
+                }
+            
+            .buttonStyle(.bordered)//estilo de boton borde
+            .foregroundColor(.black)
+            if lampara
+            {
+                Text("Me gusta el azul")
+                    .foregroundColor(.blue)
+                    .cornerRadius(130)
+                    .background(Rectangle())
+                    .cornerRadius(40)
+                    .shadow(radius: 46)
+                    .padding(2)
+                
+                    Image(systemName: "star.fill")
+                    .foregroundColor(.blue)
+            }
+            else
+            {
+                Text("Me gusta el verde ")
+                    .foregroundColor(.green)
+                    .background(Rectangle())
+                    .cornerRadius(40)
+                    .shadow(radius: 46)//relleno contorno
+                    .padding(2)
+                
+                Image(systemName: "star.fill")
+                .foregroundColor(.green)
+            }
+         }
         }
-        .padding()
     }
 }
 
@@ -24,3 +63,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
