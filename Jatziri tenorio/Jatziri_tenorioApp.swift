457f9fccaedf45a9a61831f3e13bbcba159fae0b
BereniceCastelan <jatziriten30@gmail.com>
2023-04-12 14:37:26 -0600

    Initial Commit

---
+++ Jatziri tenorio/Jatziri_tenorioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jatziri_tenorioApp.swift
+//  Jatziri tenorio
+//
+//  Created by CEDAM09 on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Jatziri_tenorioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
